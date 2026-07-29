6ac6316a018888da96cff931580c31f76b2b04a4
Muzammil Hussain <itz.muzz787@gmail.com>
2026-07-29 12:51:23 +0500

    Add cities array and log first city

---
+++ class3.js
@@ -0,0 +1,26 @@
+// var number1 = +prompt('Enter number 1');
+// var number2 = +prompt('Enter number 2');
+
+// if(number1 === number2) {
+//     console.log('both numbers are equal', number1,number2)
+// } else if(number1 > number2) {
+//     console.log('number 1 is greater than number 2', number1)
+// } else {
+//     console.log('number 2 is greater than number 2', number2)
+// }
+
+
+// var correctpassword = 'saylani';
+
+// var userpassword = prompt('Enter your password');
+
+// if(userpassword === '') {
+//     alert =('plaese enter password')
+// }
+// if(correctpassword === userpassword) {
+//     alert = ('your password is correct');
+// }
+
+var cities = ['Karachi', 'Lahore', 'Quetta'];
+
+console.log(cities[0])
